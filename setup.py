bd9067d619c5fb1ed9b42f5ea3bbcb1d74912bf0
Tom Waterman <tjwaterman99@gmail.com>
2020-09-16 14:31:32 +0000

    Bump version 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="getcohorts",
-    version="0.1.17",
+    version="1.0.0",
     author="Tom Waterman",
     author_email="tjwaterman99@gmail.com",
     description="Utilities for randomizing A/B tests",
@@ -22,6 +22,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     url="http://docs.getcohorts.com/"
 )
